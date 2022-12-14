c3765a6f1ca3896d40cdc71aaebd1cc058b85f4e
Shubhangi <sagore7004@gmail.com>
2022-12-14 15:32:13 +0530

    First commit

---
+++ src/test/java/Sample/Test1.java
@@ -0,0 +1,16 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Test1 {
+  @Test
+  public void f() {	System.setProperty("webdriver.crome.driver", "D:\\JavaStudy\\selenium\\cromedriver.exe");
+  WebDriver driver=new ChromeDriver();
+  driver.manage().window().maximize();
+  
+  driver.get("https://www.facebook.com/");
+  
+  }
+}
---
+++ testngStudy/NewTest.java
@@ -0,0 +1,13 @@
+package testngStudy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest {
+ 
+  public void f() {
+	 System.out.println("this method is excute by  testNG");
+
+  }
+}
